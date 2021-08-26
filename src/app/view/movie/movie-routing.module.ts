433e002e9913db52e49d195b2cee111052ad41e1
Diego Nascimento <diegohfn@gmail.com>
2021-08-26 16:48:21 -0400

    Cria visualização de registro de filme

--- src/app/view/movie/movie-routing.module.ts
+++ src/app/view/movie/movie-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list/movie-list.component';
+import { MovieViewComponent } from './movie-view/movie-view/movie-view.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MovieListComponent
+  },
+  {
+    path: ':id',
+    component: MovieViewComponent
   }
 ];
 
