7018987c831e902806a90cccaac2dc17168fe2a3
Diego Nascimento <diegohfn@gmail.com>
2021-08-26 16:47:22 -0400

    Atualiza  serviço para filmes
    Sobrescreve o método de solicitar um registro ao serviço para se adaptar a fonte de dados que está sendo usada.

--- src/app/service/movie/movie.service.ts
+++ src/app/service/movie/movie.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
 import { AbstractCrudService } from 'src/app/core/abstract/service/abstract-crud/abstract-crud.service';
 import { MovieModel } from 'src/app/model/movie.model';
 
@@ -10,5 +11,15 @@ export class MovieService extends AbstractCrudService<MovieModel> {
 
   constructor(http: HttpClient) {
     super(http, '/filmes.json');
+  }
+
+  getOne(id: string): Observable<any> {
+    return from(
+      this.http.get<MovieModel[]>(this.BASE_URL)
+      .toPromise()
+      .then(data => {
+        return data.find(item => item.pid === id)
+      })
+    );
   }
 }
